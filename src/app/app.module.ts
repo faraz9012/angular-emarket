b8376643f86942b91eb10f9372c812dccb1e552a
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-01-19 19:18:35 +0500

    Created header and footer, created dummy data in array to display in mini cart, added mini cart functionality. Added homepage and homepage route, added limit to display two rows in store description on homepage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontendModule } from './frontend/frontend.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FrontendModule
+    ]
 })
 export class AppModule { }
